dc2244782b073f88e65838ae65d92cc942461abe
cristiano-batista <cristiano.batista@hp.com>
2023-11-24 21:00:56 -0300

    Images preprocessing

--- README.md
+++ README.md
@@ -5,3 +5,24 @@ Esse projeto tem como objetivo principal desenvolver um dataset de imagens de pa
 O objetivo do dataset é saber quando uma folha de palma deve ser cortada ou não. 
 
 O dataset será usado para treinar e avaliar um algoritmo de aprendizado de máquina no reconhecimento de quando uma folha de palma deve ser cortada com base em imagens.
+
+
+## Pré-Processamento
+
+O pré-processamento de imagens busca criar um conjunto de dados consistente e enriquecido para treinar e testar algoritmos de aprendizado de máquina, especificamente para a classificação das palmas. 
+
+Esse processo inclui a padronização do tamanho das imagens, normalização das cores para eliminar variações indesejadas, augmentação de dados para diversificar o conjunto, e segmentação para isolar áreas de interesse, como as palmas. 
+
+O objetivo é garantir que o modelo seja treinado com dados limpos e representativos, promovendo sua eficácia na tarefa desejada.
+
+Para fazer isso, basta executar o código abaixo no terminal:
+
+``` 
+python image_preprocessing.py
+```
+
+
+Como resultado, teremos duas pastas localizada no diretório **/data/no_label: resized_data e segmented_dir.**
+
+- resized_data: contém as imagens redimensionadas;
+- segmented_dir: contém as imagens segmentadas.
---
+++ image_preprocessing.py
@@ -0,0 +1,74 @@
+import cv2 
+import os
+from PIL import Image
+import numpy as np
+
+# Indicar o caminho do dataset com as imagens originais e onde ficarão armazenadas as imagens modificadas
+root_dir = './data/no_label'
+source_dir = os.path.join(root_dir, 'dataset')
+resized_dir = os.path.join(root_dir, 'resized_data')    
+segmented_dir = os.path.join(root_dir, 'segmented_dir')    
+
+
+# Realizar o calculo da media dos valores da altura e largura das imagens
+# para redimensionar todas imagens para um mesmo tamanho
+width = []
+height = []
+
+for file in os.listdir(source_dir):
+    img_path = os.path.join(source_dir, file)
+
+    image = Image.open(img_path)
+    w, h = image.size
+
+    width.append(w)
+    height.append(h)
+
+width_mean = sum(width) / len(width)
+height_mean = sum(height) / len(height)
+
+print(f"Média de largura: {int(width_mean)}")
+print(f"Média de altura: {int(height_mean)}")
+
+width = int(width_mean)
+height = int(height_mean)
+# Definir a largura e a altura para qual as imagens serão redimensionadas
+dim = (width, height)
+
+
+# Criar a pasta para armazenar as imagens redimensionadas (caso nao exista)
+if not os.path.exists(resized_dir):
+    os.makedirs(resized_dir)
+
+# Identificar e redimensionar as imagens seguindo o padrao definido anteriormente
+for filename in os.listdir(source_dir):
+    if filename.endswith('.jpg'):
+        img = cv2.imread(os.path.join(source_dir, filename))
+        img_resized = cv2.resize(img, dim, cv2.INTER_AREA)
+        cv2.imwrite(os.path.join(resized_dir, filename), img_resized)
+    else:
+        ...
+        
+# Criar a pasta para armazenar as imagens segmentadas caso nao exista)
+if not os.path.exists(segmented_dir):
+    os.makedirs(segmented_dir)
+        
+for filename in os.listdir(resized_dir):
+
+    img = cv2.imread(os.path.join(resized_dir, filename))
+
+    # Converter a imagem de RGB para HSV
+    img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    # Definir intervalo de verde para criar a máscara
+    lower = np.array([35, 10, 10])  # Verde mais escuro
+    upper = np.array([80, 200, 200])# Verde mais claro
+
+    # Criar uma máscara usando o intervalo de cor
+    mask = cv2.inRange(img_hsv, lower, upper)
+
+    # Aplicar a máscara à imagem original
+    imageSegmented = cv2.bitwise_and(img, img, mask=mask)
+
+    # Salvar na pasta criada anteriormente as novas imagens segmentadas
+    cv2.imwrite(os.path.join(segmented_dir, filename), imageSegmented)   
